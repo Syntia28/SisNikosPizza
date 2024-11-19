2a373259a2bf216d2a58b0005eb03b4b6ba1a6ab
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2024-11-19 00:02:18 -0500

    added migrations

---
+++ SisNikosPizza.Infraestructure/Migrations/20241119031712_AddUrlImagenToProductXd.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SisNikosPizza.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUrlImagenToProductXd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "urlImagen",
+                table: "producto",
+                newName: "UrlImagen");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UrlImagen",
+                table: "producto",
+                newName: "urlImagen");
+        }
+    }
+}
