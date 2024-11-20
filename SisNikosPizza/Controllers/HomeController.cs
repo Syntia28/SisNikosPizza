0d7fb19ae28fcff034d7cb365cbaae521b681e40
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2024-11-20 17:42:07 -0500

    updated some features

--- SisNikosPizza/Controllers/HomeController.cs
+++ SisNikosPizza/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SisNikosPizza.Models;
+using SisNikosPizza.Repository.Interfaces;
 using System.Diagnostics;
 
 namespace SisNikosPizza.Controllers
@@ -7,15 +8,32 @@ namespace SisNikosPizza.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUniwork _unitWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUniwork unitWork,ILogger<HomeController> logger)
         {
             _logger = logger;
+            _unitWork = unitWork;
         }
 
-        public IActionResult Index()
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var productos = await _unitWork.ProductoRepo.ObtenerTodosAsync(ordenarPor: c => c.OrderByDescending(c => c.ProductoId));
+
+            // Construir la URL completa para cada producto
+            string baseUrl = $"{Request.Scheme}://{Request.Host}/images";
+            foreach (var producto in productos)
+            {
+                Console.Write(producto.Nombre);
+                if (!string.IsNullOrEmpty(producto.ImagenUrl))
+                {
+                    producto.ImagenUrl = $"{baseUrl}/{producto.ImagenUrl}";
+                }
+            }
+            
+            return View(productos);
         }
 
         public IActionResult Privacy()
---
+++ SisNikosPizza/README.md
@@ -0,0 +1,29 @@
+## proyecto pizzeria
+
+- [ ] SeedData: admin user and roles
+- [ ] Login: administrador
+- [x] Registro: cualquiera. )=
+- [x] Productos: administrador
+- [x] Categorias: administrador,
+- [ ] proveedores.
+- [ ] Usuarios: administrador, cambiar rol, cuando editas.
+
+version 1.1.
+
+- [ ] Pedidos. (seleccionar el cliente.)
+- [ ] Ventas.
+- [ ] Dashboard.
+
+
+- [ ] Insumoscv-.
+- [ ] Inventario.
+
+
+
+## todos 
+- [x] listar 
+- [] agregar roles.
+
+errroes
+
+- []  cuando se logea sin confirmar email no se logea.
