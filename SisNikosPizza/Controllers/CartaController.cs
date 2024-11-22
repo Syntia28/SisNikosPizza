3e191d9325668f579dca875c8810d32eb1134090
Syntia28 <n00312421@upn.pe>
2024-11-21 22:29:36 -0500

    implementar vist a principal

---
+++ SisNikosPizza/Controllers/CartaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SisNikosPizza.Domain.Models;
+using SisNikosPizza.Repository.Interfaces;
+using SisNikosPizza.Utilidades;
+
+namespace SisNikosPizza.Controllers
+{
+    public class CartaController : Controller
+
+    {
+          private readonly IUniwork _unitWork;
+    public CartaController(IUniwork unitWork)
+    {
+            _unitWork = unitWork;
+    }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var productos = await _unitWork.ProductoRepo.ObtenerTodosAsync(ordenarPor: c => c.OrderByDescending(c => c.ProductoId));
+
+            // Construir la URL completa para cada producto
+            string baseUrl = $"{Request.Scheme}://{Request.Host}/images";
+            foreach (var producto in productos)
+            {
+                Console.Write(producto.Nombre);
+                if (!string.IsNullOrEmpty(producto.ImagenUrl))
+                {
+                    producto.ImagenUrl = $"{baseUrl}/{producto.ImagenUrl}";
+                }
+            }
+
+            return View(productos);
+        }
+
+    }
+}
