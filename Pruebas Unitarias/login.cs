4cde46110641e941cd20933b8de8793a1e1b00bf
carlos21082003 <oz2003x8@gmail.com>
2025-04-24 15:39:43 -0500

    agregando proyecto de pruebas y clase de login

---
+++ Pruebas Unitarias/login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas_Unitarias
+{
+	[TestClass]
+	public class login
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
