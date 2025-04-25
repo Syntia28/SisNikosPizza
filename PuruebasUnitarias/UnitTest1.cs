48d92a4401e177f911b8b2ddea13677c4507b964
Syntia28 <n00312421@upn.pe>
2025-04-25 14:04:05 -0500

    insumos

---
+++ PuruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PuruebasUnitarias
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
