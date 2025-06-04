9c94340c19cd09b5b1cd931822e2e75289499f5d
2004Style <rubencithochavez036@gmail.com>
2025-06-04 01:48:52 -0500

    actualizacion de modelo de detalles y vistas

--- SisNikosPizza.Infraestructure/Migrations/20250604064720_actualizacionPedidoPago.Designer.cs
+++ SisNikosPizza.Infraestructure/Migrations/20250604064720_actualizacionPedidoPago.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SisNikosPizza.Infrastructure.Context;
 
@@ -11,9 +12,11 @@ using SisNikosPizza.Infrastructure.Context;
 namespace SisNikosPizza.Infraestructure.Migrations
 {
     [DbContext(typeof(SisNikosPizzaBbContext))]
-    partial class SisNikosPizzaBbContextModelSnapshot : ModelSnapshot
+    [Migration("20250604064720_actualizacionPedidoPago")]
+    partial class actualizacionPedidoPago
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SisNikosPizza.Infraestructure/Migrations/20250604064720_actualizacionPedidoPago.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SisNikosPizza.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class actualizacionPedidoPago : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Pagado",
+                table: "pedido",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pagado",
+                table: "pedido");
+        }
+    }
+}
