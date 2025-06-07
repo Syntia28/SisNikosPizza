b92d331ec6ed2da2e478bd22b60794137cd644bd
Syntia28 <n00312421@upn.pe>
2025-06-07 03:17:38 -0500

    areglo de vistas

---
+++ SisNikosPizza.Domain/ViewModels/VMDReportes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisNikosPizza.Domain.ViewModels;
+
+public class VMDReportes
+{
+public int PedidoId { get; set; }
+    public DateTime FechaPedido { get; set; }
+    public string Cliente { get; set; }
+    public int Productos { get; set; }
+    public decimal Total   { get; set; }
+}
