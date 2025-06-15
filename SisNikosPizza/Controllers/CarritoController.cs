ee4bf11e2d21a8a72e6e75fa2910ef373e37c727
2004Style <rubencithochavez036@gmail.com>
2025-06-15 00:48:54 -0500

    actualizacion

--- SisNikosPizza/Controllers/CarritoController.cs
+++ SisNikosPizza/Controllers/CarritoController.cs
@@ -21,9 +21,6 @@ namespace SisNikosPizza.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            // var carritoItems = await _unitWork.CarritoItemsRepo.ObtenerTodosAsync(ordenarPor: c => c.OrderByDescending(c => c.ProductoId), incluirPropiedades: "Producto");
-            // return View(carritoItems);
-
             var user = await _userManager.GetUserAsync(User);
             if (user == null) return RedirectToAction("Login", "Account");
 
